ec678030e104d945541a1422e218936272998413
Juan Gomez <juanpg9656@gmail.com>
2021-09-09 18:24:57 -0500

    count button new time, bepper output

--- Arduino-Uno-buttons-relay-beeper/Arduino-Uno-buttons-relay-beeper.ino
+++ Arduino-Uno-buttons-relay-beeper/Arduino-Uno-buttons-relay-beeper.ino
@@ -8,16 +8,17 @@
 #define beeper 14
 
 //define time duration in ms
-#define time1   1000
-#define time2   2000
-#define time3   3000
-#define time4   4000
+uint16_t time1 = 1000;
+uint16_t time2 = 2000;
+uint16_t time3 = 3000;
+uint16_t time4 = 4000;
 
 //function declaration
 void checkButton(int button);
 void updateState(int button);
 void trigger_relay(int button);
-void programming_mode();
+void programming_mode(int button);
+void countButtonTime(int button);
 
 int buttonState = 0;      // current state of the button
 int lastButtonState = 0;  // previous state of the button
@@ -25,16 +26,17 @@ int startPressed = 0;     // the moment the button was pressed
 int endPressed = 0;       // the moment the button was released
 int holdTime = 0;         // how long the button was hold
 int idleTime = 0;         // how long the button was idle
+uint16_t time_button_pressed = 0;
 
 void setup() {
 
   Serial.begin(9600);
-  
+
   pinMode(button1, INPUT);
   pinMode(button2, INPUT);
   pinMode(button3, INPUT);
   pinMode(button4, INPUT);
-  
+
   pinMode(relay, OUTPUT);
   pinMode(beeper, OUTPUT);
 }
@@ -48,8 +50,8 @@ void loop() {
 }
 
 /*
- * check the button status. If the status changed, then update the status
- */
+   check the button status. If the status changed, then update the status
+*/
 void checkButton(int button) {
 
   buttonState = digitalRead(button);
@@ -62,15 +64,15 @@ void checkButton(int button) {
 }
 
 /*
- * start measuring the time the button was pressed.
- * if the status changed, then calculate the time the button was pressed
- */
-void updateState(int button){
+   start measuring the time the button was pressed.
+   if the status changed, then calculate the time the button was pressed
+*/
+void updateState(int button) {
 
-  if (buttonState == HIGH){          // the button has been just pressed
+  if (buttonState == HIGH) {         // the button has been just pressed
     startPressed = millis();
   }
-  else{                              // the button has been just released
+  else {                             // the button has been just released
     endPressed = millis();
     holdTime = endPressed - startPressed;
 
@@ -81,35 +83,75 @@ void updateState(int button){
 
     if (holdTime >= 2000) {
       Serial.println("Button was held for two second or more");
-      programming_mode();
+      programming_mode(button);
     }
   }
 }
 
-void trigger_relay(int button){
-  
-  if(button == button1){
+void trigger_relay(int button) {
+
+  if (button == button1) {
     digitalWrite(relay, HIGH);
     delay(time1);
     digitalWrite(relay, LOW);
   }
-  else if(button == button2){
+  else if (button == button2) {
     digitalWrite(relay, HIGH);
     delay(time2);
     digitalWrite(relay, LOW);
   }
-  else if(button == button3){
+  else if (button == button3) {
     digitalWrite(relay, HIGH);
     delay(time3);
     digitalWrite(relay, LOW);
   }
-  else if(button == button4){
+  else if (button == button4) {
     digitalWrite(relay, HIGH);
     delay(time4);
     digitalWrite(relay, LOW);
   }
 }
 
-void programming_mode(){
+void programming_mode(int button) {
   digitalWrite(beeper, HIGH);
+
+  while (digitalRead(button4) != 1) {
+
+    buttonState = digitalRead(button);
+
+    if (buttonState != lastButtonState) { // button state changed
+      countButtonTime(button);
+    }
+    lastButtonState = buttonState;        // save state for next loop
+  }
+
+  if (programming_mode == false) {
+
+    digitalWrite(beeper, LOW);
+  }
+}
+
+void countButtonTime(int button) {
+
+  if (buttonState == HIGH) {        // the button has been just pressed
+    startPressed = millis();
+  }
+  else {                             // the button has been just released
+    endPressed = millis();
+    holdTime = endPressed - startPressed;
+    
+    // assign new times
+    if (button == button1) {
+      time1 += holdTime;
+    }
+    else if (button == button2) {
+      time2 += holdTime;
+    }
+    else if (button == button3) {
+      time3 += holdTime;
+    }
+    else if (button == button4) {
+      time4 += holdTime;
+    }
+  }
 }
