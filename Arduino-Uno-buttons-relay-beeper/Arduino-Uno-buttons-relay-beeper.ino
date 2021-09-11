5aaef8c7824f1a42cec81262afd20144529282f4
Juan Gomez <juanpg9656@gmail.com>
2021-09-11 09:18:57 -0500

    programming  mode for button 1 working with times correctly

--- Arduino-Uno-buttons-relay-beeper/Arduino-Uno-buttons-relay-beeper.ino
+++ Arduino-Uno-buttons-relay-beeper/Arduino-Uno-buttons-relay-beeper.ino
@@ -6,7 +6,7 @@
 #define relay   12
 #define beeper  7
 
-//define time duration for each button in ms
+//define time duration ms for relay depending on each button
 uint16_t time1 = 1000;
 uint16_t time2 = 2000;
 uint16_t time3 = 3000;
@@ -15,6 +15,7 @@ uint16_t time4 = 4000;
 //functions declaration
 void checkButton1();
 void updateState1();
+void checkTime1();
 void checkButton2();
 void updateState2();
 void checkButton3();
@@ -52,9 +53,10 @@ int endPressed4 = 0;       // the moment the button was released
 int holdTime4 = 0;         // how long the button was hold
 int idleTime4 = 0;         // how long the button was idle
 
+bool first_time = false;
 
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(9600);
 
   //set inputs and outputs
@@ -68,23 +70,27 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
   checkButton1();
-  checkButton2();
-  checkButton3();
-  checkButton4();
+  //checkButton2();
+  //checkButton3();
+  //checkButton4();
 }
 
 void programming_mode(int button) {
   delay(1000);
 
   digitalWrite(beeper, HIGH);
-  if (button == button1) {
-    time1 = 0;
-  }
+
   while (digitalRead(button4) != HIGH) {
     if (button == button1) {
       checkTime1();
+    }
+    if (button == button2) {
+      checkTime2();
+    }
+    if (button == button3) {
+      checkTime3();
     }
   }
   delay(1000);
@@ -96,22 +102,69 @@ void checkTime1() {
 
   buttonState1 = digitalRead(button1);
 
-  if (buttonState1 != lastButtonState1) { // button state changed
-    if (buttonState1 == HIGH) {         // the button has been just pressed
+  if (buttonState1 != lastButtonState1) {       // button state changed
+    if (buttonState1 == HIGH) {                 // the button has been just pressed
       startPressed1 = millis();
     }
-    else {                             // the button has been just released
+    else {                                     // the button has been just released
       endPressed1 = millis();
       holdTime1 = endPressed1 - startPressed1;
-      time1 += holdTime1;
+      time1 += holdTime1;                       //add time pressed to corresponging button time
+      
+      if (first_time == true) {
+        time1 = 0;
+        first_time = false;
+      }
+      
       Serial.println(time1);
     }
   }
 
-  lastButtonState1 = buttonState1;        // save state for next loop
+  lastButtonState1 = buttonState1;              // save state for next loop
 }
 
+void checkTime2() {
 
+  buttonState2 = digitalRead(button2);
+
+  if (buttonState2 != lastButtonState2) {       // button state changed
+    if (buttonState2 == HIGH) {                 // the button has been just pressed
+      startPressed2 = millis();
+    }
+    else {                                      // the button has been just released
+      endPressed2 = millis();
+      holdTime2 = endPressed2 - startPressed2;
+      time2 += holdTime2;                       //add time pressed to corresponging button time
+      Serial.println(time2);
+    }
+  }
+
+  lastButtonState2 = buttonState2;              // save state for next loop
+}
+
+void checkTime3() {
+
+  buttonState3 = digitalRead(button3);
+
+  if (buttonState3 != lastButtonState3) {       // button state changed
+    if (buttonState3 == HIGH) {                 // the button has been just pressed
+      startPressed3 = millis();
+    }
+    else {                                      // the button has been just released
+      endPressed3 = millis();
+      holdTime3 = endPressed3 - startPressed3;
+      time3 += holdTime3;                       //add time pressed to corresponging button time
+      Serial.println(time3);
+    }
+  }
+
+  lastButtonState3 = buttonState3;              // save state for next loop
+}
+
+/*
+   check if button is pressed or not. If the button is pressed, it means the status changed,
+   so enter updateState()
+*/
 void checkButton1() {
 
   buttonState1 = digitalRead(button1);
@@ -123,10 +176,10 @@ void checkButton1() {
   lastButtonState1 = buttonState1;        // save state for next loop
 }
 
-/*
-   start measuring the time the button was pressed.
-   if the status changed, then calculate the time the button was pressed
+/*  start measuring the time when the button was pressed.
+    if the status changed, then calculate the time the button was pressed
 */
+
 void updateState1() {
 
   if (buttonState1 == HIGH) {         // the button has been just pressed
@@ -143,7 +196,9 @@ void updateState1() {
 
     if (holdTime1 >= 2000) {
 
+      first_time = true;
       programming_mode(button1);
+
     }
   }
 }
